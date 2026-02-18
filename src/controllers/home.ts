b22a0d62df8c297dbe997a4e214e79291833f125
pphatdev <pphat.kits@gmail.com>
2026-02-18 13:19:29 +0700

    feat: update fullUrl in HomeController to include username and format parameters

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -7,7 +7,7 @@ export class HomeController extends Controller {
         const PORT = process.env.PORT || 3000;
         const protocol = req.protocol;
         const host = req.get('host');
-        const fullUrl = `${protocol}://${host}/`;
+        const fullUrl = `${protocol}://${host}/stats?username=pphatdev&format=webp`;
 
         const payloads = {
             ...Controller.defaultConfig,
