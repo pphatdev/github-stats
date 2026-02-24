dddc0f2fb538f15051478268e7c578a8fafbbc80
pphatdev <pphat.kits@gmail.com>
2026-02-24 16:39:55 +0700

    feat: add drizzle-studio configuration to ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -19,5 +19,14 @@ module.exports = {
             min_uptime: '10s',
             max_memory_restart: '500M'
         },
+        {
+            name: 'drizzle-studio',
+            script: 'npx',
+            args: 'drizzle-kit studio --host 127.0.0.1 --port 34222',
+            interpreter: 'none',
+            error_file: './logs/drizzle-studio-error.log',
+            out_file: './logs/drizzle-studio-output.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
     ],
 };
